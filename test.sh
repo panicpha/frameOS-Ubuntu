66c2d06f37e4ad174f4bf8e8878dfe7c8d4c1e70
panicpha <100256364+panicpha@users.noreply.github.com>
2022-03-14 00:38:17 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,32 @@ sudo apt install xfce4-appmenu-plugin
 
 #Installing Applications
 echo Coming Soon.
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+
+echo installing the must-have pre-requisites
+while read -r p ; do sudo apt-get install  $p ; done < <(cat << "EOF"
+    perl
+    zip unzip
+    exuberant-ctags
+    mutt
+    libxml-atom-perl
+    postgresql-9.6
+    libdbd-pgsql
+    curl
+    wget
+    libwww-curl-perl
+EOF
+)
+
+echo installing the nice-to-have pre-requisites
+echo you have 5 seconds to proceed ...
+echo or
+echo hit Ctrl+C to quit
+echo -e "\n"
+sleep 6
+
+sudo apt-get install -y tig
